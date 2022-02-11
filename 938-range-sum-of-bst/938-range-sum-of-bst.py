21776b35c017dee4c52fb5db2d4c2732ccfc63fb
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-02-12 04:45:27 +0900

    Time: 234 ms (70.13%), Space: 22.9 MB (17.42%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -5,7 +5,12 @@ class Solution:
             if node:
                 if low <= node.val <= high:
                     self.result += node.val
-                finder(node.left)
-                finder(node.right)
+                if node.val < low:
+                    finder(node.right)
+                elif node.val > high:
+                    finder(node.left)
+                else:
+                    finder(node.left)
+                    finder(node.right)
         finder(root)
         return self.result
