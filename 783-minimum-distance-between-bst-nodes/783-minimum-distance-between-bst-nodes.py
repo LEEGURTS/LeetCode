86d8b171ce11e9b90889305596120e0f55c72289
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-02-12 11:51:26 +0900

    Time: 34 ms (70.13%), Space: 13.8 MB (95.65%) - LeetHub

---
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.py
@@ -0,0 +1,13 @@
+class Solution:
+    result : int = sys.maxsize
+    prev : int = -sys.maxsize
+    def minDiffInBST(self, root: TreeNode) -> int:
+        if root.left:
+            self.minDiffInBST(root.left)
+
+        self.result = min(self.result, (root.val - self.prev))
+        self.prev = root.val
+        if root.right:
+            self.minDiffInBST(root.right)
+            
+        return self.result
