d60654842e575f3a5f15b6ba26764c3c13e1d1aa
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-02-26 04:09:30 +0900

    Time: 240 ms (94.70%), Space: 13.9 MB (96.98%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -4,11 +4,11 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return s[left+1:right]
+            return [left,right]
 
         if len(s) < 2 or s == s[::-1]:
             return s
-        result = ''
+        result = [0,0]
         for i in range(len(s)-1):
-            result = max(result,check(i,i+1),check(i,i+2),key = len)
-        return result
+            result = max(result,check(i,i+1),check(i,i+2),key = lambda x:(x[1]- x[0]))
+        return s[result[0]+1:result[1]]
