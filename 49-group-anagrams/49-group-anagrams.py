3ccf2928fbbeb91e28bf98d5003fd0a71330af0f
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-02-26 03:46:25 +0900

    Time: 102 ms (84.17%), Space: 17.2 MB (88.21%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,9 @@
+import collections
+
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        dict = collections.defaultdict(list)
+        for word in strs:
+            dict["".join(sorted(word))].append(word)
+        return list(dict.values())
