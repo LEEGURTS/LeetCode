49a0a85fde262ed1fc1f232b1f4553f9e149e33e
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-02-27 20:59:02 +0900

    Time: 76 ms (75.84%), Space: 13.9 MB (95.21%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,22 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        def reverse(node: ListNode, prev: ListNode = None):
-            if not node:
-                return prev
-            next, node.next = node.next, prev
-            return reverse(next, node)
-        return reverse(head)
-
-
     def addTwoNumbers(self, l1, l2) -> ListNode:
-        root = cur = ListNode(0)
-        flag = 0
-        while l1 or l2 or flag:
+        root = head = ListNode(0)
+        
+        carry = 0
+        while l1 or l2 or carry:
             sum = 0
             if l1:
                 sum += l1.val
@@ -24,11 +15,7 @@ class Solution:
             if l2:
                 sum += l2.val
                 l2 = l2.next
-            
-            flag, val = divmod(sum+flag,10)
-            cur.next = ListNode(val)
-            cur = cur.next
+            carry,val = divmod(sum+carry,10)
+            head.next = ListNode(val)
+            head = head.next
         return root.next
-    
-            
-        
