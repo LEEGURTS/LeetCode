f939a37ef771b930da617a79f0f8b762b1d45eb0
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-03-01 19:37:37 +0900

    Time: 104 ms (90.29%), Space: 17.6 MB (83.45%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,22 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+import heapq
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        root = result = ListNode(None)
-        heap = []
+    def mergeKLists(self, lists: list[ListNode]) -> ListNode:
+        q = []
         for i in range(len(lists)):
             if lists[i]:
-                heapq.heappush(heap,(lists[i].val,i,lists[i]))
-                
-        while heap:
-            node = heapq.heappop(heap)
-            index = node[1]
-            result.next = node[2]
-            
-            result = result.next
-            if result.next:
-                heapq.heappush(heap,(result.next.val,index,result.next))
-        return root.next
+                heapq.heappush(q,(lists[i].val,i,lists[i]))
+
+        root = result = ListNode()
+        while q:
+            node = heapq.heappop(q)
+            if node[2]:
+                root.next = node[2]
+            if node[2].next:
+                heapq.heappush(q,(node[2].next.val,node[1],node[2].next))
+            root = root.next
+        return result.next
