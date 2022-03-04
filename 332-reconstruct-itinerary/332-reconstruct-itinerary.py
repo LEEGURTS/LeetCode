52d7939fba59be9391e5791a705d734783347292
Geun Seong Lee <79559361+LEEGURTS@users.noreply.github.com>
2022-03-05 00:40:07 +0900

    Time: 142 ms (34.32%), Space: 14.4 MB (77.52%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -1,13 +1,16 @@
+import collections
+
+
 class Solution:
     def findItinerary(self, tickets: list[list[str]]) -> list[str]:
         result = []
         graph = collections.defaultdict(list)
-        for a, b in sorted(tickets,reverse=True):
+        for a,b in sorted(tickets):
             graph[a].append(b)
 
         def dfs(start):
             while graph[start]:
-                dfs(graph[start].pop())
+                dfs(graph[start].pop(0))
             result.append(start)
         dfs("JFK")
         return result[::-1]
